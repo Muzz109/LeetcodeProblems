129f72a7647cb08922680c6781d7fba2f3ff9a45
Muzz109 <58022822+Muzz109@users.noreply.github.com>
2020-07-02 20:58:01 -0700

    Create 141. Linked List Cycle.js
    Here we use a slow pointer and fast pointer where each when a cycle exist the slow pointer reaches the fast pointer in a cycle ...The fast pointer and slow pointer is extensively used in adding, deleting nodes in a link list

---
+++ 141. Linked List Cycle.js
@@ -0,0 +1,57 @@
+Given a linked list, determine if it has a cycle in it.
+
+To represent a cycle in the given linked list, we use an integer pos which represents the position (0-indexed) in the linked list where tail connects to. If pos is -1, then there is no cycle in the linked list.
+
+ 
+
+Example 1:
+
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where tail connects to the second node.
+
+
+Example 2:
+
+Input: head = [1,2], pos = 0
+Output: true
+Explanation: There is a cycle in the linked list, where tail connects to the first node.
+
+
+Example 3:
+
+Input: head = [1], pos = -1
+Output: false
+Explanation: There is no cycle in the linked list.
+---------------------------------------------------------------------------------------------------------------------------------------------Solution
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    
+    let slowpointer = head;
+    let fastpointer = head ;
+    
+    while(fastpointer != null && fastpointer.next != null){
+        
+        slowpointer= slowpointer.next;
+        fastpointer =  fastpointer.next.next
+          
+        if(fastpointer == slowpointer){
+            
+            return true
+        }
+        
+    }
+    return false
+};
