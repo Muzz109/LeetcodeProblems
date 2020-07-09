946fe29c68ef860780ead36ed43a58f7a4ea2661
Muzz109 <58022822+Muzz109@users.noreply.github.com>
2020-07-08 17:45:48 -0700

    Create 1290. Convert Binary Number in a Linked List to Integer.js
    The logic for the conversion of Binary to whole numbers is right to left result*2 + value

---
+++ 1290. Convert Binary Number in a Linked List to Integer.js
@@ -0,0 +1,43 @@
+iven head which is a reference node to a singly-linked list. The value of each node in the linked list is either 0 or 1. The linked list holds the binary representation of a number.
+
+Return the decimal value of the number in the linked list.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,0,1]
+Output: 5
+Explanation: (101) in base 2 = (5) in base 10
+-------------------------------------------------------------------------------------------------Solution
+
+we use Logic where the result is move from last number to the first its a result *2 + arry[n] (from right to left through a loop)
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number}
+ */
+var getDecimalValue = function(head) {
+    
+    let value = 0;
+    
+    while(head){
+        
+        value = value*2 + head.val;
+        
+        head= head.next
+    }
+    
+    return value;
+    
+   
+};
