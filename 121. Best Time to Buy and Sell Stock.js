7635de98a2b244e1f41552546561736bbb8af44e
Muzz109 <58022822+Muzz109@users.noreply.github.com>
2020-08-05 00:00:19 -0700

    Update 121. Best Time to Buy and Sell Stock.js

--- 121. Best Time to Buy and Sell Stock.js
+++ 121. Best Time to Buy and Sell Stock.js
@@ -1,1 +1,24 @@
-
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) { 
+    
+    let Maxprofit = 0;
+    
+    for(let i=0;i<prices.length;i++){
+        
+        for(let j=i+1;j<prices.length;j++){
+            
+            profit = prices[j]-prices[i]
+            
+            if(profit > Maxprofit ){
+                
+                Maxprofit = profit
+            }
+        }
+    }
+    
+    return Maxprofit;
+    
+};
